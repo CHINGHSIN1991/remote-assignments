ef4622f4eac0c3d3e3bc2f4fc4a4bb8fa4ede094
ChenChingHsin <chinghsinforwork@gmail.com>
2022-06-26 09:36:06 +0800

    Assignment3 test

--- week-3/Assignment3.js
+++ week-3/Assignment3.js
@@ -2,32 +2,48 @@ const express = require('express');
 const router = express();
 const bodyParser = require('body-parser');
 
-router.set('views','./views');
+router.set('views', './views');
 
 //增加靜態檔案的路徑
 router.use(express.static('public'));
 
 // 增加 body 解析
 router.use(bodyParser.json());
-router.use(bodyParser.urlencoded({extended:false}));
+router.use(bodyParser.urlencoded({ extended: false }));
 
 //路由
-router.get('/',function(req,res){
-    res.send('你現在進入的東西是到首頁');
-    console.log('有人造訪首頁！')    
+router.get('/', function (req, res) {
+  res.send('你現在進入的東西是到首頁');
+  console.log('有人造訪首頁！')
 })
 
-router.get('/sum.html',function(req,res){
-    res.render('sum');
+let login = function (req, res, next) {
+  let num = req.query.number;
+  let _url = req.url;
+  if (_url == '/getData') {
+    next;
+  } else if (_url !== '/getData' && num > 0) {
+    let sum = 0;
+    for(let i=0; i<= num; i++){
+      sum += i;
+    }
+    res.send(`Sum of 1 to ${num} = ${sum}`);
+  } else {
+    res.send('Wrong Parameter');
+  }
+}
+
+router.get('/getData', login, function (req, res) {
+  res.send('Lack of Parameter');
 })
 
-router.post('/sum.html',function(req,res){
-    console.log(req.body);
-    res.send('hello!!')
-    // console.log(req.body);
-    // //轉址
-    // res.redirect('search');
-    // res.render('search');
+router.post('/sum.html', function (req, res) {
+  console.log(req.body);
+  res.send('hello!!')
+  // console.log(req.body);
+  // //轉址
+  // res.redirect('search');
+  // res.render('search');
 })
 
 router.listen(3000);
